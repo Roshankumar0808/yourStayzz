932a0b42104bc3e8bf57d5df99c8c5d6181bae0a
Roshankumar0808 <roshananand2014@gmail.com>
2025-08-22 01:50:09 +0530

    Hotel Management-Security and Controllers

--- src/main/java/com/majorProject/airBnbApp/airBnbApp/controller/HotelBrowseController.java
+++ src/main/java/com/majorProject/airBnbApp/airBnbApp/controller/HotelBrowseController.java
@@ -1,4 +1,36 @@
 package com.majorProject.airBnbApp.airBnbApp.controller;
 
+import com.majorProject.airBnbApp.airBnbApp.dto.HotelInfoDto;
+import com.majorProject.airBnbApp.airBnbApp.dto.HotelInfoRequestDto;
+import com.majorProject.airBnbApp.airBnbApp.dto.HotelPriceResponseDto;
+import com.majorProject.airBnbApp.airBnbApp.dto.HotelSearchRequest;
+import com.majorProject.airBnbApp.airBnbApp.service.HotelService;
+import com.majorProject.airBnbApp.airBnbApp.service.InventoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hotels")
+@RequiredArgsConstructor
 public class HotelBrowseController {
+
+    private final InventoryService inventoryService;
+    private final HotelService hotelService;
+
+    @GetMapping("/search")
+    @Operation(summary = "Search hotels", tags = {"Browse Hotels"})
+    public ResponseEntity<Page<HotelPriceResponseDto>> searchHotels(@RequestBody HotelSearchRequest hotelSearchRequest) {
+
+        var page = inventoryService.searchHotels(hotelSearchRequest);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{hotelId}/info")
+    @Operation(summary = "Get a hotel info by hotelId", tags = {"Browse Hotels"})
+    public ResponseEntity<HotelInfoDto> getHotelInfo(@PathVariable Long hotelId, @RequestBody HotelInfoRequestDto hotelInfoRequestDto) {
+        return ResponseEntity.ok(hotelService.getHotelInfoById(hotelId, hotelInfoRequestDto));
+    }
 }
